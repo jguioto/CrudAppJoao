a10ea6d878cd184529818b97f5fd393906c2f72a
Luis Algusto <luisalgustonunesdemoraes@gmail.com>
2024-12-28 23:27:49 -0300

    Corrigindo a instalacao do EntityFrameworkCore (adicionando o --version 7.0.0)

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CrudAppJoao.Models;
+using System.Collections.Generic;
+
+namespace CrudAppJoao.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using CrudAppJoao.Data;
+using Microsoft.EntityFrame
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
